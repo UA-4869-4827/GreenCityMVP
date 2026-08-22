bc168e0441335291a35989fc3f480a7a4d0ccd1f
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2026-08-22 20:47:55 +0200

    add tests for EcoNewsDtoRequestValidator

--- core/src/test/java/greencity/validator/EcoNewsDtoRequestValidatorTest.java
+++ core/src/test/java/greencity/validator/EcoNewsDtoRequestValidatorTest.java
@@ -1,90 +1,106 @@
 package greencity.validator;
 
+import greencity.constant.ValidationConstants;
 import greencity.dto.econews.AddEcoNewsDtoRequest;
+import greencity.exception.exceptions.InvalidURLException;
 import greencity.exception.exceptions.WrongCountOfTagsException;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static greencity.ModelUtils.getAddEcoNewsDtoRequest;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ExtendWith(MockitoExtension.class)
 class EcoNewsDtoRequestValidatorTest {
     private final EcoNewsDtoRequestValidator validator = new EcoNewsDtoRequestValidator();
 
     @Test
-    void isValid_returnsTrue_whenSourceIsValid() {
+    void isValid_returnsTrue_whenSourceIsValidAndTagsAreValid() {
         AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
 
         request.setSource("https://eco-lavca.ua/");
+        request.setTags(createTags(2));
 
         assertTrue(validator.isValid(request, null));
     }
 
     @Test
-    void isValid_returnsTrue_whenSourceIsNullAndCorrectNumberOfTags() {
+    void isValid_returnsTrue_whenSourceIsNullAndTagsAreValid() {
         AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
 
         request.setSource(null);
-        request.setTags(List.of("news", "eco"));
+        request.setTags(createTags(ValidationConstants.MAX_AMOUNT_OF_TAGS));
 
         assertTrue(validator.isValid(request, null));
     }
 
     @Test
-    void isValid_returnsTrue_whenSourceIsEmptyAndCorrectNumberOfTags() {
+    void isValid_returnsTrue_whenSourceIsEmptyAndTagsAreValid() {
         AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
 
         request.setSource("");
-        request.setTags(List.of("news", "eco"));
+        request.setTags(createTags(ValidationConstants.MAX_AMOUNT_OF_TAGS));
 
         assertTrue(validator.isValid(request, null));
     }
 
     @Test
-    void isValid_returnsTrue_whenSourceIsIgnoredAndCorrectNumberOfTags() {
+    void isValid_throwsInvalidURLException_whenSourceIsInvalid() {
         AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
 
-        request.setTags(List.of("news", "eco"));
+        request.setSource("invalid-url");
+        request.setTags(createTags(ValidationConstants.MAX_AMOUNT_OF_TAGS));
+
+        assertThrowsExactly(InvalidURLException.class, () ->
+                validator.isValid(request, null));
+    }
+
+    @Test
+    void isValid_throwsWrongCountOfTagsException_whenTagsListIsEmpty() {
+        AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
+
+        request.setSource("https://eco-lavca.ua/");
+        request.setTags(List.of());
+
+        assertThrowsExactly(WrongCountOfTagsException.class, () ->
+                validator.isValid(request, null));
+    }
+
+    @Test
+    void isValid_throwsWrongCountOfTagsException_whenTagsListSizeExceedsMaxSize() {
+        AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
+
+        request.setSource("https://eco-lavca.ua/");
+        request.setTags(createTags(ValidationConstants.MAX_AMOUNT_OF_TAGS + 1));
+
+        assertThrowsExactly(WrongCountOfTagsException.class, () ->
+                validator.isValid(request, null));
+    }
+
+    @Test
+    void isValid_returnsTrue_whenTagsListSizeIsMaxAllowedSize() {
+        AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
+
+        request.setSource("https://eco-lavca.ua/");
+        request.setTags(createTags(ValidationConstants.MAX_AMOUNT_OF_TAGS));
 
         assertTrue(validator.isValid(request, null));
     }
 
     @Test
-    void isValid_throwsWrongCountOfTagsException_whenSourceIsValidAndEmptyTagsList() {
-        AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
-
-        request.setSource("https://eco-lavca.ua/");
-        request.setTags(new ArrayList<>());
-
-        assertThrowsExactly(WrongCountOfTagsException.class, () -> {
-            validator.isValid(request, null);
-        });
+    void initialize_doesNotThrowException() {
+        assertDoesNotThrow(() -> validator.initialize(null));
     }
 
-    @Test
-    void isValid_throwsWrongCountOfTagsException_whenSourceIsValidAndTagsListSizeExceedsMaxSize() {
-        AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
+    private List<String> createTags(int amountOfTags) {
+        List<String> tags = new ArrayList<>();
+        for (int i = 1; i <= amountOfTags; i++) {
+            tags.add("tag" + i);
+        }
 
-        request.setSource("https://eco-lavca.ua/");
-        request.setTags(List.of("news", "eco", "tag", "custom"));
-
-        assertThrowsExactly(WrongCountOfTagsException.class, () -> {
-            validator.isValid(request, null);
-        });
-    }
-
-    @Test
-    void isValid_returnsTrue_whenSourceIsValidAndTagsListSizeIsMaxAllowedSize() {
-        AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
-
-        request.setSource("https://eco-lavca.ua/");
-        request.setTags(List.of("news", "eco", "tag"));
-
-        assertTrue(validator.isValid(request, null));
+        return tags;
     }
 }
